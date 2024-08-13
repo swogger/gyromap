4304dff2ac3560109aba1d96649e27b1b12b13da
swogger <dimitar.kazakov@gmail.com>
2024-08-13 17:51:42 +0100

    added basic OS map

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
+import 'screens/map_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: MapScreen(),
     );
   }
 }
